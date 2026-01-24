dd2de83983bd78d660196fd8f0f5313aa34ed728
Maks Z <1178463@pw.edu.pl>
2026-01-24 15:44:01 +0100

    Added Point3, Color, Scene, Sphere

--- include/Sphere.hpp
+++ include/Sphere.hpp
@@ -1,15 +1,20 @@
 #pragma once
 #include "Point3.hpp"
+#include "Color.hpp"
 
 class Sphere
 {
 public:
-    Sphere() : origin(0.0, 0.0, 0.0), diameter(1.0) {}
-    Sphere(Point3 origin, float diameter)
-        : origin(origin), diameter(diameter) {}
+    Sphere() : origin(0.0, 0.0, 0.0), diameter(1.0), color(0,0,0) {}
+    Sphere(Point3 origin, float diameter, Color color)
+        : origin(origin), diameter(diameter), color(color) {}
         
     float diameter;
     Point3 origin;
+    Color color;
+
 private:
 
 };
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Sphere, diameter, origin, color);
